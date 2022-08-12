c676a0b74e5fc1b3e55e48bb82194bde43cb0184
Michael Metcalf <mikegmac14@gmail.com>
2022-08-11 17:50:53 -0700

    Add ability to change size of grid with a slider

--- index.js
+++ index.js
@@ -17,12 +17,35 @@ const gridOne = document.createElement("div");
 const gridOne = document.createElement("div");
 */
 const container = document.querySelector(".container");
-makeGrid(256);
+const slider = document.querySelector(".grod");
+const slideOut = document.querySelector(".output");
+
+
+
+
+slideOut.innerHTML = slider.value;
+
+slider.oninput = function() {
+    slideOut.innerHTML = this.value;
+    let value = this.value;
+    console.log(slider.value);
+  }
+
 
 function makeGrid(num){
-for(let i = 1; i <= num; i++){
+    container.style.gridTemplateColumns = `repeat(${num} , 1fr)`;
+    container.style.gridTemplateRows = `repeat(${num} , 1fr)`;
+    let squars = container.querySelectorAll("div");
+
+    let total = num * num;
+
+for(let i = 1; i <= total; i++){
     let gridPart = document.createElement("div");
     gridPart.className = "gridPart";
     container.appendChild(gridPart);
 }
+}
+
+function changeSize(input){
+    makeGrid(input);
 }
